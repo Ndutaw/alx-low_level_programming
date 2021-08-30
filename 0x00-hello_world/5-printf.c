b9fe84b5548a2ffcfaf84152e1d4afc0e11f6bdd
Ndutaw <ndutaw94@gmail.com>
2021-08-30 13:53:31 +0000

    c lang

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - main function
+ * Main - Main function of the project
  *
  * return - return the value 0
  */
 int main(void)
-{ 
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
