ef03f7198fa43d3a44634f3c90e351c88dc1f3f5
Ndutaw <ndutaw94@gmail.com>
2021-08-30 13:26:47 +0000

    c lang

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,11 +7,14 @@
  * *
  * *
  * *Return: return 0 everytime
- * */
+ *
+ */
 int main(void)
 {
 
+	
 	char displayed[52];
+
 	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
 	puts(displayed);
 	return (0);
