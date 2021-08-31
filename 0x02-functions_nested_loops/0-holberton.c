d666bd6083b39d1c7f8b1628a65856018a164d75
Ndutaw <ndutaw94@gmail.com>
2021-08-31 21:15:16 +0000

    loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * main - main function of my project
- *
- * Return: always return 0
- */
+ * *main - function prints text as output
+ * *
+ * *Return:return 0
+ * */
 int main(void)
 {
-	char c[10] = "Holberton";
+	char school[10] = "_putchar";
 	int i;
-	for(i = 0; i < 10; i++)
-       	{
-	_putchar(c[i]);
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
 	}
-	_putchar("\n");
+	_putchar(10);
 	return (0);
 }
