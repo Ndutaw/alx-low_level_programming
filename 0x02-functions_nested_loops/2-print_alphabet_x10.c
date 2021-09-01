0ce90b372d8352ab4ed119b95358ed013faea09a
Ndutaw <ndutaw94@gmail.com>
2021-09-01 22:06:17 +0000

    loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/** print_alphabet - prints alphabet 10 times
+ *
+ * return: no return
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 1; 1 <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 a program that prints HOLBERTON followed by a new line
 function that prints the alphabet followed by a new line
+prints 10 times the alphabet in lowercase followed by a new line
