12fe04f6762c749fbc5411c1960e6148ed016089
Ndutaw <ndutaw94@gmail.com>
2021-09-08 22:31:43 +0000

    pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,22 +6,10 @@
  */
 void swap_int(int *a, int *b)
 {
-	int a;
-	int b;
-
-	a = 98;
-	b = 42;
-
 	{
-		_putchar ("a=%d, b=%d\n", a, b);
-		swap_int(&a, &b);
-		_putchar ("a=%d, b=%d\n", a, b);
-	}
-	void swap (int *a, int *b)
-	{
-		int temp;
-		temp = *a;
+		int tmp;
+		tmp = *a;
 		*a = *b;
-		*b = temp;
+		*b = tmp;
 	}
 }
