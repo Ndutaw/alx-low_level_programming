f20a6fd67124fc2f587de5c0fde75c95a7bde245
Ndutaw <ndutaw94@gmail.com>
2021-09-09 17:24:08 +0000

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,6 +6,7 @@
 void print_rev(char *s)
 {
 	int len = 0, index = 0;
+
 	len = _strlen(s);
 	
 	for (index = len - 1; index >= 0; index--)
@@ -22,6 +23,7 @@ void print_rev(char *s)
 int _strlen(char *s)
 {
 	int count, inc;
+
 	inc = 0;
 	for (count = 0; s[count] != '\0'; count++)
 		inc++;
