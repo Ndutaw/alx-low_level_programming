8e78a91ad53be1cdb58683d8462b4d1dbe37660e
Ndutaw <ndutaw94@gmail.com>
2021-09-09 15:06:39 +0000

    pointer

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- * _strlen - returns length of a string
+ * int_strlen - returns length of a string
  * @s: string
  * return: returns length
  */
 int _strlen(char *s)
 {
 	int count, inc;
+
 	inc = 0;
+
 	for (count = 0; s[count] != '\0'; count++)
 		inc++;
 
