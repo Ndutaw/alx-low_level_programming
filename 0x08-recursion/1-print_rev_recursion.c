fdbfa423ab706b4527bf56907fe67020c846f529
Ndutaw <ndutaw94@gmail.com>
2021-09-17 20:48:55 +0000

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * _print_rev_recursion - function that prints a string in reverse
  * @s: parameter string
  *
  */
+
 void _print_rev_recusrion(char *s)
 {
 	if (*s != '\0')
